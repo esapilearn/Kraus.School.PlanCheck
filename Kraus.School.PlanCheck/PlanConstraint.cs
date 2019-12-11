2fef189205f3d2077b87f9c8cd2c19895f4f229d
james.a.kraus196@gmail.com <james.a.kraus196@gmail.com>
2019-12-11 15:51:21 -0800

    Updated PlanConstraint and MainViewModel

--- Kraus.School.PlanCheck/PlanConstraint.cs
+++ Kraus.School.PlanCheck/PlanConstraint.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ESAPIX.Constraints;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,29 @@ using System.Threading.Tasks;
 
 namespace Kraus.School.PlanCheck
 {
-    class PlanConstraint
+    public class PlanConstraint : BindableBase
     {
+        public PlanConstraint(IConstraint con)
+        {
+            this.Constraint = con;
+        }
+
+        private IConstraint _constraint;
+
+        public IConstraint Constraint
+        {
+            get { return _constraint; }
+            set { SetProperty(ref _constraint, value); }
+        }
+
+        private ConstraintResult _result;
+
+        public ConstraintResult Result
+        {
+            get { return _result }
+            set { _result = value; }
+        }
+
+
     }
 }
--- Kraus.School.PlanCheck/ViewModels/MainViewModel.cs
+++ Kraus.School.PlanCheck/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using ESAPIX.Common;
+using Prism.Commands;
 using Prism.Mvvm;
+using System.Collections.ObjectModel;
 using System.Linq;
 using VMS.TPS.Common.Model.API;
 
@@ -11,54 +13,12 @@ namespace Kraus.School.PlanCheck.ViewModels
 
         public MainViewModel()
         {
-            //Initialize with first loaded plan if any
-            OnPlanChanged(VMS.GetValue(sac => sac.PlanSetup));
-            //Handle plan changes (in standalone mode)
-            VMS.Execute(sac => sac.PlanSetupChanged += OnPlanChanged);
+
         }
 
-        public void OnPlanChanged(PlanSetup ps)
-        {
-            //Must operate on VMS thread for plan access
-            VMS.Invoke(() =>
-            {
-                Id = ps?.Id;
-                UID = ps?.UID;
-                IsDoseCalculated = ps?.Dose != null;
-                NBeams = ps?.Beams.Count();
-            });
-        }
+        public ObservableCollection<PlanConstraint> Constraints { get; set; } = new ObservableCollection<PlanConstraint>();
+        public DelegateCommand EvaluateCommand { get; set; }
 
-        private string id;
 
-        public string Id
-        {
-            get { return id; }
-            set { SetProperty(ref id, value); }
-        }
-
-        private string uid;
-
-        public string UID
-        {
-            get { return uid; }
-            set { SetProperty(ref uid, value); }
-        }
-
-        private int? nBeams;
-
-        public int? NBeams
-        {
-            get { return nBeams; }
-            set { SetProperty(ref nBeams, value); }
-        }
-
-        private bool isDoseCalculated;
-
-        public bool IsDoseCalculated
-        {
-            get { return isDoseCalculated; }
-            set { SetProperty(ref isDoseCalculated, value); }
-        }
     }
 }
