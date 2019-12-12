f6879a5d367057843c065e6281f36974b281a2d0
james.a.kraus196@gmail.com <james.a.kraus196@gmail.com>
2019-12-11 17:16:55 -0800

    Added test build

---
+++ Kraus.School.PlanCheckTests1/CTDateConstraintTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kraus.School.PlanCheck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Facade.API;
+using ESAPIX.Constraints;
+
+namespace Kraus.School.PlanCheck.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void ConstrainTestPassesTest()
+        {
+            var im = new Image();
+            im.CreationDateTime = DateTime.Now.AddDays(-59);
+            var expected = ResultType.PASSED;
+            var actual = new CTDateConstraint().Constrain(im).ResultType;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ConstrainTestFailsTest()
+        {
+            var im = new Image();
+            im.CreationDateTime = DateTime.Now.AddDays(-61);
+            var expected = ResultType.ACTION_LEVEL_3;
+            var actual = new CTDateConstraint().Constrain(im).ResultType;
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
